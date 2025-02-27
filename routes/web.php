1ee6835b86774a845f365e33bf0bcfac7be858d1
Yuki <kokado.yuki@gmail.com>
2025-02-27 11:41:43 +0700

    remove dashboard route

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,6 @@ Route::get('/posts/edit', function () {
     return view('posts.edit');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
